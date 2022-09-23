2431a8b329e00d836b972d20d2e9ed40c1b489b7
Gabriel lujano salas <glujano@MacBook-Pro-de-Gabriel.local>
2022-09-23 10:34:25 -0500

    trigger container

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,3 +1,4 @@
+//test workflow
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
