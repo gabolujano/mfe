c968cf70c317ef218f015bcbe7ca6fee2d0eac15
Gabriel lujano salas <glujano@MacBook-Pro-de-Gabriel.local>
2022-09-24 13:03:31 -0500

    add auth subapp v2

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,3 +1,4 @@
+//trigger
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
